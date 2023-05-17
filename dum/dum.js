0fe7667ed63bdf482e1b183e4012996db12b3e39
Agusgomez912 <101596571+Agusgomez912@users.noreply.github.com>
2023-05-17 17:01:02 -0300

    Add files via upload

---
+++ dum/dum.js
@@ -0,0 +1,22 @@
+/*
+document.write("esta funcionando")
+var elementoboton=document.getElementById("boton1")
+var subtitulo=document.getElementById("subtitulo")
+//cambiamos el elemento
+
+var texto1=prompt("Ingrese el nombre del boton")
+var color=prompt("Ingrese el color del subtitulo")
+
+elementoboton.innerHTML=texto1
+subtitulo.style.color=color
+
+//
+
+//cambiamos el color del texto del elento
+
+//prompt("ingrese un color")*/
+
+
+//creamos un nuevo elemento parrafo
+var nuevoparrafo=document.createElement("p")
+//establecemos el contenido
